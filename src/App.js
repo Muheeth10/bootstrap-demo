7ebc6c33c154c0f59394f65786c2c35e6f90ff2f
Muheeth10 <muheeth17@gmail.com>
2021-01-14 13:24:39 +0530

    Initial setup of the bootsrap5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="par">
+      <h1 className='text-center' >Hello World</h1>    
+      </div>
+      <div className="card" style={{width: "18rem"}}>
+        <img src="https://picsum.photos/seed/picsum/300/200" className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+      
     </div>
   );
 }
